024881dcabf8cb441406cd26db36b39faaa889c9
helzaaragao <helza.batista.aragao@gmail.com>
2023-02-02 11:12:30 -0300

    atualizações

--- imperativa/aula_5.js
+++ imperativa/aula_5.js
@@ -1,42 +1,46 @@
 // Vizualização de como o codigo está sendo executado
 console.log("Hello World"); 
+console.log('Hello, Word!');
 
+//Tipos primitivos (Number,String, Boolean)
+/* Number 
+    + -> Adição 
+    - -> Subtração
+    / -> Divisão
+    * -> Multiplacação
+    % -> Resto da divisão -> 5%2 -> 2*2 + 1 = 5 então o resultado é 1 
+    ** -> Exponenciação -> 2**3 -> 8 ( dois ao cubo)
+    RaizQuadrada -> 4**0.5 -> Raiz de 4 -> 2
+*/
 
-//number 
-console.log(1 + 5); 
-console.log(9*8);
+console.log(1 + 5);  //6
+console.log( 9 * 8 ); // 72
 
-
-/* 
-   string 
-   + - junção 
-
+/* String ou Texto  
+    + -> Concanetação ou junção ( prioriza a string)
+    Nan = Not a Number 
 */
 console.log("Meu texto 1" + " " + "Meu texto 2");
+console.log('1' + 2); // 12 porque junta ( prioriza a string)
+console.log('1' - 2);  // -1, não junta e consegue transformar em número
+console.log('1a' - 2) // NaN, erro porque não reconhece o primeiro 
 
-// - Variavéis -- Armazenar alguma informação na memoria do computador
 
-/* 
+/* Variavéis -- Armazenar alguma informação na memoria do computador
    const -- Constante - um valor definido que não pode ser mais alterado
-
    let -- pode mudar o valor 
-
-   - and & pode iniciar na nomeclatura = &outraVariavel
-
-   var - mais antigo do let 
+   var - mais antigo do let, não recomendado mais utilizar se não for requerido
+    - and $ pode iniciar na nomeclatura = &outraVariavel
 */
-
-const MINUTO = 60; 
-const MINHA_VARIAVEL = "minha alegria"
-
+const MINUTO = 60; // quando é const em um projeto grande costuma está em maiuscula para deixar mais evidente 
+const MINHA_VARIAVEL = "minha alegria"; 
+const HORA = 60*MINUTO; 
+const DIA = 24+HORA; 
 let outraVariavel = "Meu texto"; 
 outraVariavel = "Meu texto trocado agora"; 
-
-/* 
-
-   let - pega só no escopo que foi feito + pode mudar 
-   var - visibilidade global, pode ser acessada fora do escopo de declaração
-*/ 
+console.log(outraVariavel);
+/* let - pega só no escopo que foi feito + pode mudar 
+   var - visibilidade global, pode ser acessada fora do escopo de declaração */ 
 
 
 
