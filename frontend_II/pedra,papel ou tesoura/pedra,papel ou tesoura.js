a5eef8bb85a1817b1015b6d68d00533d91d78d0b
helzaaragao <helza.batista.aragao@gmail.com>
2023-02-21 17:08:10 -0300

    ajeitando

--- frontend_II/pedra,papel ou tesoura/pedra,papel ou tesoura.js
+++ frontend_II/pedra,papel ou tesoura/pedra,papel ou tesoura.js
@@ -1,27 +1,29 @@
 
-
-/*Implementar uma regra de que só é declarado como ganhador, quem ganhou 2 rodadas, para auxiliar use uma variável para armazenar o vencedor de cada rodada;
-Empate no jogo, não dá pontuação para nenhum dos jogadores.
-
-*/
-
+// a função que precisa para o jogo funcionar
 function playGame() {
+    //Armazenar os scores 
     let playerWins = 0; 
     let computerWins = 0; 
 
+    // Definir que enquanto ninguém tiver atingido 2 pontos, o jogo segue
     while (playerWins < 2 && computerWins < 2){ 
+        //Dando as opções para o jogador
         const playerChoice = prompt("Jogador (a)! Você escolhe pedra, papel ou tesoura?"); 
-
+        
+        //caso o jogador erre as opções
         if (playerChoice !=="pedra" && playerChoice !== "papel" && playerChoice !== "tesoura"){ 
             alert("Epa! Você não sabe jogar? Essa resposta não está entre as opções!"); 
             continue;
         }
 
+        // As opções que são validas
         const OPCOES = [ "pedra", "papel", "tesoura"];
+        // A resposta do computador 
         const COMPUTEROPCAO = OPCOES[Math.floor(Math.random() * 3)]; 
     
-
+        // resultado vai ser amazenado
         let result; 
+        // O que irá devolve depende do resultado 
         if ( playerChoice === COMPUTEROPCAO) { 
             result = "Parece que você leu  os dados do nosso computador, deu empate!"; 
         } else if (
@@ -34,10 +36,12 @@ function playGame() {
             result = "O computador ganhou essa rodada de você";
         }
 
+        // Avisando o jogador do resultado completo
         alert(`Você escolheu ${playerChoice}, o nosso computador escolheu ${COMPUTEROPCAO}. ${result}`);
 
     }
-
+    
+    // Finalizando com os alertes finais de vencedor ou perdedor
     if (playerWins > computerWins){ 
         alert("Você ganhou o jogo! Parabéns!");
     } else { 
@@ -45,5 +49,6 @@ function playGame() {
     }
 }
 
+//utilizando a função
 playGame();
 
